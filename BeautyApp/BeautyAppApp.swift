281da0f8f533dcb93698ab2536344b0f4dd96174
Emil Shafigin <emilshafigin@Emils-MacBook-Pro.local>
2023-04-15 00:24:02 +0600

    Initial commit

---
+++ BeautyApp/BeautyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeautyAppApp.swift
+//  BeautyApp
+//
+//  Created by Emil Shafigin on 15/4/23.
+//
+
+import SwiftUI
+
+@main
+struct BeautyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
